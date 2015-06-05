f799e0915e886ea8e4e0b8182b637ed18947f69c
Kevin Jones <kevin@rocksolidknowledge.com>
2015-06-05 09:59:47 +0100

    Initial project files

---
+++ src/main/com/mantiso/proxy/console/Proxy.java
@@ -0,0 +1,10 @@
+package com.mantiso.proxy.console;
+
+/**
+ * Created by kevin on 05/06/2015.
+ */
+public class Proxy {
+    public static void main(String args[]){
+        System.out.println("Hello, World");
+    }
+}
